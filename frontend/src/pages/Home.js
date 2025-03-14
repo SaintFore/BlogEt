5eb47f335213202840a4393d2ca689a69373ce4c
ShuiMuLuo <forevergoodforest@outlook.com>
2025-03-15 01:19:40 +0800

    添加基础页面组件

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>欢迎来到我的博客</h1>
+      <p>这里将展示最新的博客文章列表</p>
+      {/* 博客文章列表将在完成API后添加 */}
+    </div>
+  );
+}
+
+export default Home;
